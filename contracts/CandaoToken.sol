0c6b7491168e56dc513438dd9a725839db0b77b6
daosrepository <105541358+daosrepository@users.noreply.github.com>
2022-06-06 11:14:19 +0200

    reduce interfaces
    no interface for callbacks

--- contracts/CandaoToken.sol
+++ contracts/CandaoToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "./RecoverableFunds.sol";
-import "./interfaces/ICallbackContract.sol";
+//import "./interfaces/ICallbackContract.sol";
 //import "./WithCallback.sol";
 //import "@openzeppelin/constracts/token/ERC20/utils/SafeERC20.sol";
 /**
