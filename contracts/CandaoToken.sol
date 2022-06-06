095dacea88f6de8b9d997363f5f912e6df8af7e6
daosrepository <105541358+daosrepository@users.noreply.github.com>
2022-06-06 10:45:18 +0200

    reduce import
    import WithCallbacks is now reduced

--- contracts/CandaoToken.sol
+++ contracts/CandaoToken.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "./RecoverableFunds.sol";
 import "./interfaces/ICallbackContract.sol";
-import "./WithCallback.sol";
+//import "./WithCallback.sol";
 //import "@openzeppelin/constracts/token/ERC20/utils/SafeERC20.sol";
 /**
  * @dev CandaoToken
