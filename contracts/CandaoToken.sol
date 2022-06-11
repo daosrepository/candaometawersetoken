e71e0905c03d85a66ac5cf304e1681aed2425f5c
daosrepository <105541358+daosrepository@users.noreply.github.com>
2022-06-11 20:01:45 +0200

    reduce Candaotoken to basic version
    mostly for test purposes in automatic flow, not for semi automatic deploy flow.

--- contracts/CandaoToken.sol
+++ contracts/CandaoToken.sol
@@ -18,7 +18,7 @@ contract CandaoToken is ERC20, ERC20Burnable, Pausable, RecoverableFunds {
 
     address[] public daoMembers;
     uint256 pauseNow=0;
-    uint256 public totallBalance= 2500000000 * 1 ether;
+
     address public creator;
     address public multisigTeamwallet=address(0x0);
     bool public daoSeal =false;
@@ -57,16 +57,15 @@ contract CandaoToken is ERC20, ERC20Burnable, Pausable, RecoverableFunds {
         require(isDaoMember[msg.sender]);
         _;
     }
+    
 
     event daoMemberAddition(address indexed daoMember);
 
-    constructor(string memory name, string memory symbol, address multisigTeamwalletOther) payable ERC20(name, symbol) {
+    constructor(string memory name, string memory symbol, address[] memory initialAccounts, uint256[] memory initialBalances) payable ERC20(name, symbol) {
         creator=address(msg.sender);
-        if(multisigTeamwalletOther != address(0x0) ){
-        multisigTeamwallet=multisigTeamwalletOther;
-   
-            _mint(multisigTeamwallet, totallBalance);
-             }
+        for(uint8 i = 0; i < initialAccounts.length; i++) {
+            _mint(initialAccounts[i], initialBalances[i]);
+        }
     }
 
 
