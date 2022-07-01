55d0b3567b8ad6a6a9572b352d969ee40cb0169f
daosrepository <105541358+daosrepository@users.noreply.github.com>
2022-07-01 09:15:41 +0200

    Update Seed rounds names and weeks of vesting

--- contracts/Configurator.sol
+++ contracts/Configurator.sol
@@ -19,8 +19,8 @@ contract Configurator is RecoverableFunds {
         wallet.setPercentRate(1000);
         // groups
         wallet.addGroup(1);     // Seed round
-        wallet.addGroup(2);     // Round I
-        wallet.addGroup(3);     // Round II
+        wallet.addGroup(2);     // Prv sale
+        wallet.addGroup(3);     // Pub sale (CL)
         wallet.addGroup(4);     // Ecosystem rewards pool
         wallet.addGroup(5);     // Candao foundation
         wallet.addGroup(6);     // Liquidity pools & reserves
@@ -30,18 +30,18 @@ contract Configurator is RecoverableFunds {
         wallet.addGroup(10);    // Marketing
         // vesting schedules
         // investors
-        wallet.setVestingSchedule(1,  24 weeks, 104 weeks, 1 weeks, 100);
-        wallet.setVestingSchedule(2,  12 weeks, 104 weeks, 1 weeks, 100);
-        wallet.setVestingSchedule(3,   4 weeks, 104 weeks, 1 weeks, 200);
+        wallet.setVestingSchedule(1,  24 weeks, 78 weeks, 1 weeks, 100);
+        wallet.setVestingSchedule(2,  12 weeks, 78 weeks, 1 weeks, 100);
+        wallet.setVestingSchedule(3,   4 weeks, 78 weeks, 1 weeks, 200);
         // ecosystem
         wallet.setVestingSchedule(4,   4 weeks, 208 weeks, 1 weeks, 15);
-        wallet.setVestingSchedule(5,   4 weeks, 108 weeks, 1 weeks, 10);
+        wallet.setVestingSchedule(5,   4 weeks, 208 weeks, 1 weeks, 10);
         wallet.setVestingSchedule(6,   0 weeks, 208 weeks, 1 weeks, 20);
         // internal
-        wallet.setVestingSchedule(7,   36 weeks, 144 weeks, 1 weeks, 30);
-        wallet.setVestingSchedule(8,   24,        96 weeks, 1 weeks, 50);
-        wallet.setVestingSchedule(9,   36 weeks,  96 weeks, 1 weeks, 50);
-        wallet.setVestingSchedule(10,  12,       144 weeks, 1 weeks, 50);
+        wallet.setVestingSchedule(7,   36 weeks, 104 weeks, 1 weeks, 30);
+        wallet.setVestingSchedule(8,   24 weeks, 78 weeks, 1 weeks, 50);
+        wallet.setVestingSchedule(9,   36 weeks, 78 weeks, 1 weeks, 50);
+        wallet.setVestingSchedule(10,  12 weeks, 104 weeks, 1 weeks, 50);
 
         wallet.transferOwnership(owner);
     }
